772aec3b49d684b4bfaeae7113153eea7f2cd976
Formerscholar <2319315514@qq.com>
2021-01-05 21:40:24 +0800

    搭建自动api文档，整体架构

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('nestjsApi文档')
+    .setDescription('自动生成文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,24 +1,90 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags, ApiProperty } from '@nestjs/swagger';
+
+
+class CreatePostDto {
+  @ApiProperty({
+    description: '帖子标题'
+  })
+  title: string
+  @ApiProperty({
+    description: '帖子内容'
+  })
+  content: string
+}
+
+class UpdatePostDto {
+  @ApiProperty({
+    description: '帖子id'
+  })
+  id: string
+  @ApiProperty({
+    description: '帖子标题'
+  })
+  title: string
+  @ApiProperty({
+    description: '帖子内容'
+  })
+  content: string
+}
+
+class ByIdPostDto {
+  @ApiProperty({
+    description: '帖子id'
+  })
+  id: string
+}
 
 @Controller('post')
+@ApiTags('帖子')
 export class PostController {
   @Get()
+  @ApiOperation({
+    summary: '显示帖子列表'
+  })
   index() {
     return []
   }
 
   @Post()
-  create() {
+  @ApiOperation({
+    summary: '创建帖子'
+  })
+  create(@Body() body: CreatePostDto) {
+
     return {
-      msg: 'success'
+      msg: 'success',
+      data: body
     }
   }
 
-  @Get(':id')
-  detail() {
+  @Get('detail')
+  @ApiOperation({ summary: '帖子详情' })
+  detail(@Query('id') id: string) {
     return {
-      id: 1,
-      title:'123'
+      id,
+      title: '123'
+    }
+  }
+
+
+  @Put('update')
+  @ApiOperation({ summary: '编辑帖子' })
+  update(@Body() body: UpdatePostDto) {
+    return {
+      msg: 'success',
+      data: body
+    }
+  }
+
+  @Delete('remove')
+  @ApiOperation({
+    summary: '删除帖子'
+  })
+  remove(@Body() body: ByIdPostDto) {
+    return {
+      body,
+      msg: 'success'
     }
   }
 
